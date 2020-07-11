fc2ecd3f2d4be5868be78abe5bac598d64625c63
tianzhi03 <tianzhi03@kuaishou.com>
2020-07-11 16:24:34 +0800

    initial commit

---
+++ e2e/plugin-e2e/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'plugin-e2e',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/e2e/plugin-e2e',
+};
---
+++ packages/plugin/src/schematics/plugin/schema.d.ts
@@ -0,0 +1,5 @@
+export interface PluginSchematicSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
